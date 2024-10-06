c86e265f3f1f057689ad143dc2c6fa6cfac727bc
tarun <tarun1940c@gmail.com>
2024-10-06 23:39:13 +0530

    uguguiyuiuyi

---
+++ kandelys.java
@@ -0,0 +1,20 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	
+	public static long maxSubarraySum(int[] arr, int n) {
+		// write your code here
+		Double max = Double.NEGATIVE_INFINITY;;
+		long sum =0;
+		for (int i=0;i<n;i++){
+			sum+=arr[i];
+			max = Math.max(max,sum);
+			if(sum<0){
+				sum =0;
+			}
+		}
+		return max;
+	}
+
+}
